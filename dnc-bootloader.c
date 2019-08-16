7bd175ffd627e9c610f2a2295c981d8cf03b49bf
Daniel J Blueman <daniel@numascale.com>
2019-08-16 14:15:39 +0800

    Fix build warnings with newer toolchains

--- dnc-bootloader.c
+++ dnc-bootloader.c
@@ -2576,7 +2576,7 @@ static void probefilter_check(void)
 			if (en != pf) {
 				char buf[32];
 				snprintf(buf, sizeof(buf), "%s/%03x ", get_master_name(node->sci), node->sci);
-				strncat(msg, buf, sizeof(buf));
+				strcat(msg, buf);
 				break;
 			}
 		}
